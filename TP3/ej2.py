2ac64ab5e08cc04bc7b73debcc4186d17ffa9d4b
SebasF1349 <ratablanca711@gmail.com>
2023-09-10 10:19:30 -0300

    agregado docstring

--- TP3/ej2.py
+++ TP3/ej2.py
@@ -1,4 +1,13 @@
 # Implemente una función que dada una lista de nombres, devuelva una nueva lista que contenga solo los nombres que tengan una longitud mayor o igual a una cantidad de caracteres pasada por parámetro, utilizando list comprehensions.
 
 def nombres_mayores(nombres, caracteres):
+    """Selecciona elementos de un arreglo mayores a una cantidad de caracteres especificados
+
+    Args:
+        nombres (list): lista de la cual filtrar valores
+        caracteres (number): menor longitud de caracteres a seleccionar
+
+    Returns:
+        list: lista con elementos filtrados
+    """
     return [nombre for nombre in nombres if len(nombre) >= caracteres]
