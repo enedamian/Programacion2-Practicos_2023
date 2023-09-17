e12807a241c42545905d37a3ef1e5ad52ad14dff
not-tomi <peruilhtomas22@gmail.com>
2023-09-17 20:42:41 -0300

    mejore la identacion

--- TP3/TP3_punto1.py
+++ TP3/TP3_punto1.py
@@ -2,8 +2,6 @@ def cuadrado(num):
     listanum = [valor**2 for valor in num]
     return listanum
 
+
 llenar_lista = [int(input("Ingrese un valor: ")) for _ in range(5)]
 print(cuadrado(llenar_lista))
-
-""" profe no termine de entender en llenar_lista como es que se llena la lista """
-""" probando algo con los commits"""
