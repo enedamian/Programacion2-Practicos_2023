0d172f33c1063b754abe3357a6e20919d550121b
Gianluca Fagherazzi <fagherazzigianluca@gmail.com>
2023-09-24 18:32:53 -0300

    comentario nuevo

--- TP3/Punto8.py
+++ TP3/Punto8.py
@@ -4,6 +4,8 @@ contenga solo los elementos únicos utilizando list comprehensions."""
 mi_lista = [1, 2, 2, 3, 4, 4, 5, 6, 6, 1]
 
 #utilizo set para crear un set de elementos unicos desordenados, y luego los meto en una lista con list
+#set te crea un conjunto de elementos
 nueva_lista = list(set(mi_lista))
 
 print(nueva_lista)
+
