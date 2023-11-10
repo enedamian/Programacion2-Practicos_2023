8acfc1b86aaef6af0a7773ea92a65e9d60a4f574
FMartine7i <frmmartinetti98@gmail.com>
2023-11-10 11:45:34 -0300

    Actividad empezada

---
+++ TP6/API_viajes/destinos.py
@@ -0,0 +1,47 @@
+destinos = {
+    1: {
+        'name': 'Berlin',
+        'price': 500,
+        'country': 'Germany'
+        },
+    2: {
+        'name': 'Milan',
+        'price': 450,
+        'country': 'Italy'
+        },
+    3: {
+        'name': 'Barcelona',
+        'price': 350,
+        'country': 'Spain'
+        },
+    4: {
+        'name': 'Los Angeles',
+        'price': '500',
+        'country': 'USA'
+        },
+    5: {
+        'name': 'Cancún',
+        'price': 400,
+        'country': 'Mexico'
+        },
+    6: {
+        'name': 'Bogotá',
+        'price': 300,
+        'country': 'Colombia'
+        },
+    7: {
+        'name': 'Buenos Aires',
+        'price': 600,
+        'country': 'Argentina'
+        },
+    8: {
+        'name': 'Kyoto',
+        'price': 550,
+        'country': 'Japan'
+        },
+    9: {
+        'name': 'Seúl',
+        'price': 450,
+        'country': 'Korea'
+        }
+}
