a208061e1490c6afa7d66e8bfe90cd0fd9532e89
FMartine7i <frmmartinetti98@gmail.com>
2023-11-11 09:05:27 -0300

    cambios

--- TP6/API_viajes/app.py
+++ TP6/API_viajes/app.py
@@ -14,43 +14,28 @@ def obtener_viaje(id):
         return jsonify({id: destinos[id]})
     return jsonify({'error': 'viaje no encontrado'}), 404
 
-@app.route('/login/', methods = ['GET'])
-def show_login_form_get():
-    return 'No estás logeado. Esto es un GET'
-
-@app.route('/viajes/nuevo', methods = ['POST'])
+@app.route('/viajes', methods = ['POST'])
 def agregar_viaje():
-    if request.method == 'POST':
-        nuevo_destino = {
-            'name': request.form['name'],
-            'price': request.form['price'],
-            'country': request.form['country']
-        }
-        nuevo_id = max(destinos.keys()) + 1
-        destinos[nuevo_id] = nuevo_destino
-        return jsonify(nuevo_destino)
-    else:
-        return jsonify({'error': 'No se pudo agregar el viaje'})
+    nuevo_destino = {
+        'name': request.form['name'],
+        'price': request.form['price'],
+        'country': request.form['country']
+    }
+    nuevo_id = max(destinos.keys()) + 1
+    destinos[nuevo_id] = nuevo_destino
+    return jsonify(nuevo_destino)
+
     
 
-@app.route('/viajes/<int:id>/<string:destino>', methods = ['PUT'])
-def actualizar_viaje(id, destino):
-    if id not in destinos:
+@app.route('/viajes/<int:id>', methods = ['PUT'])
+def actualizar_viaje(id):
+    viaje = [destino for destino in destinos if destino["id"] == id]
+    if len(viaje)==0:
         return jsonify({'error': 'No existe un viaje con ese ID'})
-    elif destino in destinos:
-        return jsonify({'error': 'Ya existe un destino con ese nombre'})
     else:
-        destinos[id] = destino
-        return jsonify(f'viaje {id} con destino a {destino} actualizado correctamente')
+        # modificar el destino
+        return jsonify(f'viaje {id} con destino a {viaje["name"]} actualizado correctamente')
 
-@app.route('/login/', methods = ['POST'])
-def show_login_form():
-    if 'name' in request.form:
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
-        return 'Hola ' + name + ' ' + email + ' ' + password
-    return 'No estás logeado. Esto es un POST'
 
 @app.route('/')
 def index():
